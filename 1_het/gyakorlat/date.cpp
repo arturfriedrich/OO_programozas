ac77c555a4473fcafe786a9f1b305c1db280fd51
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-09-09 13:55:52 +0200

    fixed bug

--- 1_het/gyakorlat/date.cpp
+++ 1_het/gyakorlat/date.cpp
@@ -1,6 +1,8 @@
 #include <iostream> // iostream-et eleg itt include-olni a headerben nem kell!
 #include "date.h"
 
+using namespace std;
+
 Date::Date(int day, int month, int year) {
 	_month = _is_month_admissible(month) ? month : 0;
 	_day = _is_day_admissible(day, month, year) ? day : 0;
@@ -8,31 +10,29 @@ Date::Date(int day, int month, int year) {
 }
 
 void Date::add_days(int days) {
-    int daysLeft = days;
-    while (true) {  // ameddig vannak napok adddig folytatjuk
-        int leftInMonth = _days_left_in_month(_day, _month, _year);
-        int newDaysLeft = daysLeft - leftInMonth;
-        if (newDaysLeft == 0) {
-            _day = _number_of_days_in_month(_month, _year);
-            return;
-        }
-        if (newDaysLeft < 0) {
-            _day = _day + daysLeft;
-            return;
-        }
-        // léptetni kell mert túl sok nap van hátra
-        daysLeft = newDaysLeft;
-        _month = _month + 1 < 13 ? _month + 1 : 1;
-        _year = _month == 1 ? _year + 1 : _year;
-        _day = 1;
-    }
+	int daysLeft = days;
+	while (true) { // ameddig meg vannak napok, folytatjuk
+		int leftInMonth = _days_left_in_month(_day, _month, _year);
+		int newDaysLeft = daysLeft - leftInMonth;
+		if (newDaysLeft == 0) {
+			_day = _number_of_days_in_month(_month, _year);
+			return;
+		}
+		if (newDaysLeft < 0) {
+			_day = _day + daysLeft;
+			return;
+		}
+		// leptetni kell, mert tul sok nap van hatra!
+		daysLeft = newDaysLeft - 1;
+		_month = _month + 1 < 13 ? _month + 1 : 1;
+		_year = _month == 1 ? _year + 1 : _year;
+		_day = 1;
+
+	}
 }
 
 void Date::print() {
-	std::cout <<
-		monthNames[_month] << " " <<
-		_day << ", " <<
-		_year << std::endl;
+	cout << monthNames[_month] << " " << _day << ", " << _year << endl;
 }
 
 std::string Date::monthNames[] = {
