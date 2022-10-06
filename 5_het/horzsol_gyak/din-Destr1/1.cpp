521fd4e079a3791e8c6c2d4d241838bd2edd3ce5
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-10-06 18:31:35 +0200

    5. heti horzsol gyak kommentelve

--- 5_het/horzsol_gyak/din-Destr1/1.cpp
+++ 5_het/horzsol_gyak/din-Destr1/1.cpp
@@ -1,46 +1,48 @@
 #include "Decl.hpp"
 
 void dm_kiir::kiir(int i) const
- {
-    /* nem tudja elérni a változót, ezért használja a gettert
-       csak úgy érheti el a gettert, ha pointert használ az eredeti osztályra
-       a -> jel egy balra kötő operátor, akkor működik, ha a pont bal oldalán egy név áll, ezért itt
-       a pont itt nem használható, a nyilat akkor kell használnom, ha a bal oldalán egy cím áll
-       név.metódus        (ugyanabban az osztályban)
-       cím->metódus       (nem ugyanabban az osztályban)
-       ez akkor történhet amikor a metódus egy másik osztályban van, nem abban emylikben működtetni szeretnénk */
-  for(int j=0; j<mutat->getIsm(); j++)     /* Soron belüli kiírás */
-   {
-    if(i<mutat->getSor()) cout << mutat->getKar();
-    else cout << "-";
-   }
+{
+		/* nem tudja elérni a változót, ezért használja a gettert
+			csak úgy érheti el a gettert, ha pointert használ az eredeti osztályra
+			a -> jel egy balra kötő operátor, akkor működik, ha a pont bal oldalán egy név áll, ezért itt
+			a pont itt nem használható, a nyilat akkor kell használnom, ha a bal oldalán egy cím áll
+			név.metódus        (ugyanabban az osztályban)
+			cím->metódus       (nem ugyanabban az osztályban)
+			ez akkor történhet amikor a metódus egy másik osztályban van, nem abban emylikben működtetni szeretnénk */
+	for ( int j=0; j<mutat->getIsm(); j++ )       	              /* Soron belüli kiírás */
+	{
+		if (i<mutat->getSor()) cout << mutat->getKar();
+		else cout << "-";
+	}
  }
 
 sor_minta& sor_minta::setIsm(int v_im)
- {
-  if((ism+v_im)>0 and (ism+v_im)<11) ism+=v_im;
-  else ism=10;
-  return *this;
- }
+{
+	if ( (ism+v_im)>0 and (ism+v_im)<11 ) ism+=v_im;
+	else ism=10;
+	return *this;
+}
+
 sor_minta& sor_minta::setSor(int v_sr)
- {
-  if((sor+v_sr)>0 and (sor+v_sr)<5) sor+=v_sr;
-  else sor=4;
-  return *this;
- }
+{
+	if ( (sor+v_sr)>0 and (sor+v_sr)<5 ) sor+=v_sr;
+	else sor=4;
+	return *this;
+}
+
 sor_minta& sor_minta::setKar(char v_kr)
- {
-  if(v_kr!=kar) kar=v_kr;
-  else kar='!';
-  return *this;
- }
+{
+	if ( v_kr!=kar ) kar=v_kr;
+	else kar='!';
+	return *this;
+}
   
 void mind(const dm_kiir& nyzj, dm_kiir& es, dm_kiir& zzj, int s_K)
- {
-  cout << "\n" << endl;
-  for(int i=0; i<s_K; i++)              /* Sorok kiírása */
-   {
-    nyzj.kiir(i); es.kiir(i); zzj.kiir(i);
-    cout << "\n" << endl;
-   }
- }
+{
+	cout << "\n" << endl;
+	for ( int i=0; i<s_K; i++ )              						  /* Sorok kiírása */
+	{
+		nyzj.kiir(i); es.kiir(i); zzj.kiir(i);
+		cout << "\n" << endl;
+	}
+}
--- 5_het/horzsol_gyak/din-Destr1/Decl.hpp
+++ 5_het/horzsol_gyak/din-Destr1/Decl.hpp
@@ -4,61 +4,59 @@
 using namespace std;
 
 /* ez az osztály csak a tagváltozókat, gettereket és settereket tartalmazza, valamint a konstruktorokat, nincsenek destrukturok */
-class sor_minta /* Osztály deklarációja */
- {
-   private:
-    char kar; /* kiirandó karakter */
-    int ism;  /* soron belüli ismétlődések száma */
-    int sor;  /* sorok száma, amennyiben megjelenjen */
-   public:
-   /* ha nincsen lényege, el lehet hagyni a konstruktort, de azért érdemes */
-    sor_minta() { kar='A'; ism=1; sor=1; }  /* Alapértelmezett konstruktor  */
-    sor_minta(char kr, int im, int sr)  /* Alapérték beállító konstruktor  */
-     { kar=kr; ism=im; sor=sr; }
-	 int getIsm() const { return ism; }
-   int getSor() const { return sor; }
-   char getKar() const { return kar; }
-	 sor_minta& setIsm(int v_im);
-	 sor_minta& setSor(int v_sr);
-	 sor_minta& setKar(char v_kr);
-     ~sor_minta() { cout << "!!!" << endl; }
+class sor_minta 			/* Osztály deklarációja */
+{
+	private:
+    	char kar; 			/* kiirandó karakter */
+    	int ism;  			/* soron belüli ismétlődések száma */
+    	int sor;  			/* sorok száma, amennyiben megjelenjen */
+   	public:
+   		/* ha nincsen lényege, el lehet hagyni a konstruktort, de azért érdemes */
+    	sor_minta() { kar='A'; ism=1; sor=1; }  		/* Alapértelmezett konstruktor  */
+    	sor_minta(char kr, int im, int sr) { 			/* Alapérték beállító konstruktor  */
+     		kar=kr; ism=im; sor=sr; 
+		}
+	 	int getIsm() const { return ism; }
+   		int getSor() const { return sor; }
+   		char getKar() const { return kar; }
+		sor_minta& setIsm(int v_im);
+		sor_minta& setSor(int v_sr);
+		sor_minta& setKar(char v_kr);
+     	/* ~sor_minta() { cout << "!!!" << endl; } */
  };
 
 /* ez egy idegen osztály */
 class dm_kiir
- {
-  private:
-  /* mivel alapvetően nem látszódik a másik osztály, itt rá kell mutatnunk 
-     ez az osztály, nem fogja közvetlenül elérni a másik osztály tagváltozóit, csak a muatató segítségével */
-   sor_minta* mutat;
-   const int sK;
-  public:
-   dm_kiir() : sK(4) /* Alapértelmezett konstruktor + inicializáló lista  */
-    { 
-    /* a new szó után az alapértelmezett konstruktorra hivatkozom, úgy foglalok 
-       dinamikus memóriaterületet egy idegen osztályban, hogy a default osztályra hivatkozok */
-     mutat = new sor_minta();
-     cout << "Az alapértelmezett objektum címe: ";
-     cout << mutat << endl;
-    }
-   dm_kiir(char kr, int im, int sr) : sK(4) /* Alapérték beállító konstruktor + inicializáló lista */
-    { 
-     /* itt is egy konstruktort hívok, megadunk neki paramétereket is */
-     mutat = new sor_minta(kr, im, sr);
-     cout << "A paraméteres objektum címe: ";
-     cout << mutat << endl;
-    }
-   void kiir(int i) const ;
-   int getsK() const { return sK; }
-   /* visszaadja azt a memóriacímet amire a mutató mutat, ami jelenleg az osztály objektuma */
-   sor_minta* getMutat() const { return mutat; }
-   /* felszabadítja az objektumoknak odaadott memóriacímeket */
-   ~dm_kiir()                                       /* Destruktor */
-     { 
-      delete mutat;
-      cout << "Felszabadítottam az objektum memóriacímét, amely a: ";
-      cout << mutat << endl;
-     }
+{
+  	private:
+		/* mivel alapvetően nem látszódik a másik osztály, itt rá kell mutatnunk 
+			ez az osztály, nem fogja közvetlenül elérni a másik osztály tagváltozóit, csak a muatató segítségével */
+		sor_minta* mutat;
+		const int sK;
+  	public:
+   		dm_kiir() : sK(4) {								/* Alapértelmezett konstruktor + inicializáló lista  */
+			/* a new szó után az alapértelmezett konstruktorra hivatkozom, úgy foglalok 
+			dinamikus memóriaterületet egy idegen osztályban, hogy a default osztályra hivatkozok */
+			mutat = new sor_minta();
+			cout << "Az alapértelmezett objektum címe: ";
+			cout << mutat << endl;
+    	}
+   		dm_kiir(char kr, int im, int sr) : sK(4) { 		/* Alapérték beállító konstruktor + inicializáló lista */
+			/* itt is egy konstruktort hívok, megadunk neki paramétereket is */
+			mutat = new sor_minta(kr, im, sr);
+			cout << "A paraméteres objektum címe: ";
+			cout << mutat << endl;
+    	}
+		void kiir(int i) const;
+		int getsK() const { return sK; }
+   		/* visszaadja azt a memóriacímet amire a mutató mutat, ami jelenleg az osztály objektuma */
+		sor_minta* getMutat() const { return mutat; }
+		/* felszabadítja az objektumoknak odaadott memóriacímeket */
+   		~dm_kiir() {                                    /* Destruktor */ 
+			delete mutat;
+			cout << "Felszabadítottam az objektum memóriacímét, amely a: ";
+			cout << mutat << endl;
+     	}
  };
  
  /* const objektumot csak const metódus tud kezelni */
