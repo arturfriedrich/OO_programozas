2917db5b8e39fb566a77ba2b63c80d0a8478d383
Friedrich Artúr <artur.friedrich.harka@gmail.com>
2022-10-14 11:20:22 +0200

    refactored

--- 4_het/horzsol_gyak/proba_zh_v2/V2/Decl.hpp
+++ 4_het/horzsol_gyak/proba_zh_v2/V2/Decl.hpp
@@ -4,22 +4,22 @@
 using namespace std;
 
 class tri_race /* Osztály deklarációja */
- {
-   private:
-    string lic;
-    int swim, cycle, run, depo, ij;
-    int sum, h, p, mp;
-    const int oRA, pRC;
-   public:
-    tri_race() : oRA(3600), pRC(60)
-     { lic="--------"; swim=0; cycle=0; run=0; depo=0; ij=0;\
-       h=0; p=0; mp=0; sum=0; }
-    tri_race(string lc, int sw, int cyc, int rn, int dp, int t) : oRA(3600), pRC(60)
-     { lic=lc; swim=sw; cycle=cyc; run=rn; depo=dp; ij=t; h=0; p=0; mp=0; sum=0; }
-   int getIj() const { return ij; }
-   tri_race& kiir_rajt();
-   tri_race& kiir_cel();
-	 tri_race& setSum(int t);
-   ~tri_race()
-    { cout << "\nFelszabadítottam az objektum memóriacímét!"; }
-	};
+{
+	private:
+		string lic;
+		int swim, cycle, run, depo, ij;
+		int sum, h, p, mp;
+		const int oRA, pRC;
+	public:
+		tri_race() : oRA(3600), pRC(60)
+			{ lic="--------"; swim=0; cycle=0; run=0; depo=0; ij=0;\
+		       h=0; p=0; mp=0; sum=0; }
+		tri_race(string lc, int sw, int cyc, int rn, int dp, int t) : oRA(3600), pRC(60)
+			{ lic=lc; swim=sw; cycle=cyc; run=rn; depo=dp; ij=t; h=0; p=0; mp=0; sum=0; }
+	int getIj() const { return ij; }
+	tri_race& kiir_rajt();
+	tri_race& kiir_cel();
+	tri_race& setSum(int t);
+	~tri_race()
+		{ cout << "\nFelszabadítottam az objektum memóriacímét!"; }
+};
