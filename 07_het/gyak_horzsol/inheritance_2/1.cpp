32c4bdc2cf010ff35d786b71cc0d946c55cf7aad
Friedrich Artúr <artur.friedrich.harka@gmail.com>
2022-10-20 14:07:39 +0200

    added inhertitance_2

---
+++ 07_het/gyak_horzsol/inheritance_2/1.cpp
@@ -0,0 +1,48 @@
+#include "Decl.hpp"
+
+void tri_race::kiir_i1()
+ { cout << h << ":" << p << ":" << mp  << " [h:m:s]\n"; }
+
+void tri_race::kiir_i2()
+ { cout << "Licence: " << lic << "\nÚ: " << swim << "\nB: " << cycle << "\nD: " << depo; }
+
+void tri_race::kiir_i3()
+ { cout << "\nF: " << run  << "\nT-: " << ij << "\nCél idő: "; }
+
+void in_ch::kiir_i4()
+ { kiir_i2();
+   cout << "\nÚ+B+D: " << swim+cycle+depo;
+   kiir_i3(); kiir_i1();
+   cout << "Kategória: " << kat << endl; }
+
+tri_race& tri_race::kiir_rajt()
+ { cout << "\nSzülő rajt idő: "; kiir_i1(); return *this; }
+ 
+tri_race& tri_race::kiir_cel()
+ { kiir_i2(); kiir_i3(); kiir_i1(); return *this; }
+ 
+in_ch& in_ch::kiir_rajt()
+ { cout << "\nGyerek rajt idő: "; kiir_i1(); return *this; }
+ 
+in_ch& in_ch::kiir_cel()
+ { kiir_i4(); return *this; }
+  
+in_gr_ch& in_gr_ch::kiir_rajt()
+ { cout << "\nUnoka rajt idő: "; kiir_i1(); return *this; }
+
+in_gr_ch& in_gr_ch::kiir_cel()
+ { kiir_i4();
+   cout << "Nem: " << nem << endl;
+   return *this; }
+  
+tri_race& tri_race::setSum(int t)
+ { sum=swim+cycle+run+depo-t;
+   h=sum/oRA; p=(sum%oRA)/pRC; mp=(sum%oRA)%pRC;
+   return *this; }
+
+void mind1(tri_race* x_tri)                                         /* ez nem referencia, dinamikus memóriát használok, ezért ez egy cím */
+ { cout << endl;
+   x_tri->kiir_rajt().setSum(x_tri->getIj()).kiir_cel(); }          /* itt -> használunk mert címre hivatkozom */
+   
+void felsz(tri_race* x_tri)                                         /* nem tagfüggvénnyel szabadítom fel */
+ { delete x_tri; }
---
+++ 07_het/gyak_horzsol/inheritance_2/main.cpp
@@ -0,0 +1,19 @@
+#include "1.cpp"
+
+int main(void)
+ {
+  tri_race* alap=new tri_race;												/* dinamikus memóriát hozunk az adatszerkezetek létrehozására */
+  tri_race* A_tri=new tri_race("M_V_1357",1200,4200,2300,123,60);			/* itt nem egy név lesz, hanem egy memóriacím */
+  tri_race* B_tri=new tri_race("V_G_2468",1300,4300,2400,125,0);
+  tri_race* C_tri=new tri_race("B_G_9048",1400,4400,2400,130,90);
+  in_ch* D_tri=new in_ch("C_H_1234",1450,4400,2400,130,30,5);
+  in_gr_ch* E_tri=new in_gr_ch("G_R_4321",1500,4450,2350,120,0,4,'F');
+  mind1(alap); mind1(A_tri); mind1(B_tri); mind1(C_tri);
+  mind1(D_tri); mind1(E_tri);
+  // delete alap;										/* a delete hozza működésbe a destruktort */
+  // delete A_tri; delete B_tri; delete C_tri;			/* szülők felszabadítása */
+  // delete D_tri;										/* gyerek felszabadítása */
+  // delete E_tri;										/* unoka felszabadítása */
+  felsz(alap); felsz(A_tri); felsz(B_tri); felsz(C_tri); felsz(D_tri); felsz(E_tri);		/* felszabadítások, a kapcsolatot a pointer biztosítja */
+  return 0;
+}
