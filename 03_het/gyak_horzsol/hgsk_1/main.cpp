940c90ddb78c3a7891a3a5194e45d0be8e972b8c
Friedrich Artúr <artur.friedrich.harka@gmail.com>
2022-10-21 13:33:03 +0200

    03_het refactored

--- 03_het/gyak_horzsol/hgsk_1/main.cpp
+++ 03_het/gyak_horzsol/hgsk_1/main.cpp
@@ -2,39 +2,39 @@
 #include "Decl.hpp"
 
 void sor_minta::kiir(int i)
- {
-  for(int j=0; j<ism; j++)                /* Soron belüli kiírás */
-   {
-    if(i<sor) cout << kar;
-    else cout << "-";
-   }
- }
+{
+	for (int j=0; j<ism; j++)			/* Soron belüli kiírás */
+	{
+		if (i<sor) cout << kar;
+		else cout << "-";
+	}
+}
 
 void sor_minta::v_i()
- {
-  do {
-      cout << "\n Kérem a változtatás mértékét [+/-]: ";
-      cin >> v_m;
-     } while((ism+v_m)<=0);
-  ism+=v_m;
- }
+{
+	do {
+		cout << "\n Kérem a változtatás mértékét [+/-]: ";
+		cin >> v_m;
+		} while((ism+v_m)<=0);
+	ism+=v_m;
+}
  
- void mind(sor_minta nyzj, sor_minta es, sor_minta zzj)
- {
-  cout << "\n" << endl;
-  for(int i=0; i<SK; i++)              /* Sorok kiírása */
-   {
-    nyzj.kiir(i); es.kiir(i); zzj.kiir(i);
-    cout << "\n" << endl;
-   }
- }
+void mind(sor_minta nyzj, sor_minta es, sor_minta zzj)
+{
+	cout << "\n" << endl;
+	for (int i=0; i<SK; i++)			/* Sorok kiírása */
+	{
+		nyzj.kiir(i); es.kiir(i); zzj.kiir(i);
+		cout << "\n" << endl;
+	}
+}
  
 int main(void)
- {
-  sor_minta nyzj('[',2,2), es('&',3,3), zzj(']',4,1), alap;      /* Objektumok példányosítása, beállítása */
-  alap.elemek(); es.elemek(); cout << "\n" << endl; 
-  mind(nyzj,es,zzj);
-  es.v_i();
-  mind(nyzj,es,zzj);
-  return 0;
+{
+	sor_minta nyzj('[',2,2), es('&',3,3), zzj(']',4,1), alap;		/* Objektumok példányosítása, beállítása */
+	alap.elemek(); es.elemek(); cout << "\n" << endl; 
+	mind(nyzj,es,zzj);
+	es.v_i();
+	mind(nyzj,es,zzj);
+	return 0;
 }
--- 03_het/gyak_horzsol/hgsk_4/main.cpp
+++ 03_het/gyak_horzsol/hgsk_4/main.cpp
@@ -2,40 +2,40 @@
 #include "Decl.hpp"
 
 void sor_minta::kiir(int i)
- {
-  for(int j=0; j<ism; j++)                /* Soron belüli kiírás */
-   {
-    if(i<sor) cout << kar;
-    else cout << "-";
-   }
- }
+{
+	for (int j=0; j<ism; j++)			/* Soron belüli kiírás */
+	{
+		if (i<sor) cout << kar;
+		else cout << "-";
+	}
+}
 
 sor_minta& sor_minta::setIsm(int im)
- {
-  do {
-      cout << "\n Kérem a változtatás mértékét [1-10]: " << im << " +-= ";
-      cin >> v_m;
-     } while((im+v_m)<=0 or (im+v_m)>10);
-  ism+=v_m;
-  return *this;
- }
+{	
+	do {
+		cout << "\n Kérem a változtatás mértékét [1-10]: " << im << " +-= ";
+		cin >> v_m;
+		} while ((im+v_m)<=0 or (im+v_m)>10);
+	ism+=v_m;
+	return *this;
+}
  
- void mind(sor_minta nyzj, sor_minta es, sor_minta zzj)
- {
-  cout << "\n" << endl;
-  for(int i=0; i<SK; i++)              /* Sorok kiírása */
-   {
-    nyzj.kiir(i); es.kiir(i); zzj.kiir(i);
-    cout << "\n" << endl;
-   }
- }
+void mind(sor_minta nyzj, sor_minta es, sor_minta zzj)
+{
+	cout << "\n" << endl;
+	for (int i=0; i<SK; i++)			/* Sorok kiírása */
+	{
+		nyzj.kiir(i); es.kiir(i); zzj.kiir(i);
+		cout << "\n" << endl;
+	}
+}
  
 int main(void)
- {
-  sor_minta nyzj('[',2,2), es('&',3,3), zzj(']',4,1), alap;     /* Objektumok példányosítása, beállítása */
-  cout << alap.getIsm() << "\n" << es.getIsm()  << "\n" << endl; 
-  mind(nyzj,es,zzj);
-  es.setIsm(es.getIsm());
-  mind(nyzj,es,zzj);
-  return 0;
+{
+	sor_minta nyzj('[',2,2), es('&',3,3), zzj(']',4,1), alap;		/* Objektumok példányosítása, beállítása */
+	cout << alap.getIsm() << "\n" << es.getIsm()  << "\n" << endl; 
+	mind(nyzj,es,zzj);
+	es.setIsm(es.getIsm());
+	mind(nyzj,es,zzj);
+	return 0;
 }
