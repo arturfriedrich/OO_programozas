939db1e13c8a81303fff72230445894569922ad9
Friedrich Artúr <artur.friedrich.harka@gmail.com>
2022-10-28 13:22:06 +0200

    din_cast_2 refactored

--- 08_het/gyak_horzsol/din_cast_2/1.cpp
+++ 08_het/gyak_horzsol/din_cast_2/1.cpp
@@ -1,44 +1,46 @@
 #include "Decl.hpp"
 
 void tri_race::kiir_i1() const
- { cout << h << ":" << p << ":" << mp  << " [h:m:s]\n"; }
+ 	{ cout << h << ":" << p << ":" << mp  << " [h:m:s]\n"; }
 
 void tri_race::kiir_i2() const
- { cout << "Licence: " << lic << "\nÚ: " << swim << "\nB: " << cycle << "\nD: " << depo; }
+ 	{ cout << "Licence: " << lic << "\nÚ: " << swim << "\nB: " << cycle << "\nD: " << depo; }
 
 void tri_race::kiir_i3() const
- { cout << "\nF: " << run  << "\nT-: " << ij << "\nCél idő: "; }
+ 	{ cout << "\nF: " << run  << "\nT-: " << ij << "\nCél idő: "; }
 
-void in_ch::kiir_i4() const
- { kiir_i2();
-   cout << "\nÚ+B+D: " << swim+cycle+depo;
-   kiir_i3(); kiir_i1();
-   cout << "Kategória: " << kat << endl; }
+void in_ch::kiir_i4() const { 
+	kiir_i2();
+	cout << "\nÚ+B+D: " << swim+cycle+depo;
+	kiir_i3(); kiir_i1();
+	cout << "Kategória: " << kat << endl; 
+}
 
 tri_race& tri_race::kiir_rajt()
- { cout << "\nSzülő rajt idő: "; kiir_i1(); return *this; }
+ 	{ cout << "\nSzülő rajt idő: "; kiir_i1(); return *this; }
  
 tri_race& tri_race::kiir_cel()
- { kiir_i2(); kiir_i3(); kiir_i1(); return *this; }
+ 	{ kiir_i2(); kiir_i3(); kiir_i1(); return *this; }
  
 in_ch& in_ch::kiir_rajt()
- { cout << "\nGyerek rajt idő: "; kiir_i1(); return *this; }
+ 	{ cout << "\nGyerek rajt idő: "; kiir_i1(); return *this; }
  
 in_ch& in_ch::kiir_cel()
- { kiir_i4(); return *this; }
+ 	{ kiir_i4(); return *this; }
   
 in_gr_ch& in_gr_ch::kiir_rajt()
- { cout << "\nUnoka rajt idő: "; kiir_i1(); return *this; }
+ 	{ cout << "\nUnoka rajt idő: "; kiir_i1(); return *this; }
 
 in_gr_ch& in_gr_ch::kiir_cel()
  { kiir_i4();
    cout << "Nem: " << nem << endl;
    return *this; }
   
-tri_race& tri_race::setSum(int t)
- { sum=swim+cycle+run+depo-t;
-   h=sum/oRA; p=(sum%oRA)/pRC; mp=(sum%oRA)%pRC;
-   return *this; }
+tri_race& tri_race::setSum(int t) { 
+	sum=swim+cycle+run+depo-t;
+	h=sum/oRA; p=(sum%oRA)/pRC; mp=(sum%oRA)%pRC;
+	return *this; 
+}
 
 void mind1(tri_race* x_tri)
 	{ 
@@ -64,4 +66,4 @@ void mind1(tri_race* x_tri)
 	}
    
 void felsz(tri_race* x_tri)
- { delete x_tri; cout << x_tri; x_tri=0; }
+ 	{ delete x_tri; cout << x_tri; x_tri=0; }
--- 08_het/gyak_horzsol/din_cast_2/Decl.hpp
+++ 08_het/gyak_horzsol/din_cast_2/Decl.hpp
@@ -3,63 +3,67 @@
 
 using namespace std;
 
-class tri_race // szülő osztály deklarációja
- {
-   protected:
-    string lic;
-    int swim, cycle, run, depo, ij, h, p, mp, sum;
-   private:
-    const int oRA, pRC;
-   public:
-    tri_race() : oRA(3600), pRC(60), h(0), p(0), mp(0), sum(0)
-      { lic="--------"; swim=0; cycle=0; run=0; depo=0; ij=0;
-        cout << "\nAz alapértelmezett szülő-objektum létrejött: ";}  
-    tri_race(string lc, int sw, int cyc, int rn, int dp, int t) : oRA(3600), pRC(60),\
-             h(0), p(0), mp(0), sum(0)
-     { lic=lc; swim=sw; cycle=cyc; run=rn; depo=dp; ij=t;
-      cout << "\nA paraméteres szülő-objektum létrejött: "; }
-   int getIj() const { return ij; }
-   string getLic() const { return lic; }
-   void kiir_i1() const;
-   void kiir_i2() const;
-   void kiir_i3() const;
-   virtual tri_race& kiir_rajt();
-   virtual tri_race& kiir_cel();
-	 tri_race& setSum(int t);
-   virtual ~tri_race()
-    { cout << "\nFelszabadítottam a szülő-objektum memóriacímét: "; }
-   tri_race(const tri_race&) = delete;
-   tri_race& operator=(const tri_race&) = delete;	
-	};  
+class tri_race						/* szülő osztály deklarációja */
+{
+   	protected:
+		string lic;
+		int swim, cycle, run, depo, ij, h, p, mp, sum;
+   	private:
+		const int oRA, pRC;
+	public:
+		tri_race() : oRA(3600), pRC(60), h(0), p(0), mp(0), sum(0) { 
+			lic="--------"; swim=0; cycle=0; run=0; depo=0; ij=0;
+			cout << "\nAz alapértelmezett szülő-objektum létrejött: ";
+		}  
+		tri_race(string lc, int sw, int cyc, int rn, int dp, int t) : oRA(3600), pRC(60),\
+				h(0), p(0), mp(0), sum(0) { 
+			lic=lc; swim=sw; cycle=cyc; run=rn; depo=dp; ij=t;
+			cout << "\nA paraméteres szülő-objektum létrejött: "; 
+		}
+		int getIj() const { return ij; }
+		string getLic() const { return lic; }
+		void kiir_i1() const;
+		void kiir_i2() const;
+		void kiir_i3() const;
+		virtual tri_race& kiir_rajt();
+		virtual tri_race& kiir_cel();
+		tri_race& setSum(int t);
+		virtual ~tri_race()
+			{ cout << "\nFelszabadítottam a szülő-objektum memóriacímét: "; }
+		tri_race(const tri_race&) = delete;
+		tri_race& operator=(const tri_race&) = delete;	
+};  	
 	
-class in_ch : public tri_race // származtatott gyerek osztály deklarációja
+class in_ch : public tri_race 		/* származtatott gyerek osztály deklarációja */
  {
     int kat;
-   public:
-    in_ch() : tri_race() { kat=0; }
-    in_ch(string lc, int sw, int cyc, int rn, int dp, int t, int kt) : \
-           tri_race(lc, sw, cyc, rn, dp, t)
-     { kat=kt; cout << "\nA paraméteres gyerek-objektum létrejött: "; }
-   void kiir_i4() const;
-   virtual in_ch& kiir_rajt();
-   virtual in_ch& kiir_cel();
-   virtual ~in_ch()
-    { cout << "\nFelszabadítottam a gyerek-objektum saját adatának a memóriacímét: "; }
+   	public:
+		in_ch() : tri_race() { kat=0; }
+		in_ch(string lc, int sw, int cyc, int rn, int dp, int t, int kt) : \
+				tri_race(lc, sw, cyc, rn, dp, t) { 
+			kat=kt; cout << "\nA paraméteres gyerek-objektum létrejött: "; 
+		}
+		void kiir_i4() const;
+		virtual in_ch& kiir_rajt();
+		virtual in_ch& kiir_cel();
+		virtual ~in_ch()
+			{ cout << "\nFelszabadítottam a gyerek-objektum saját adatának a memóriacímét: "; }
  };
  
-class in_gr_ch : public in_ch  // származtatott unoka osztály deklarációja 
- {
-   private:
-    char nem;
-   public:
-    in_gr_ch() : in_ch(), nem('N') {  }
-    in_gr_ch(string lc, int sw, int cyc, int rn, int dp, int t, int kt, char nm) : \
-           in_ch(lc, sw, cyc, rn, dp, t, kt), nem(nm)
-     { cout << "\nA paraméteres unoka-objektum létrejött: "; }
-   virtual in_gr_ch& kiir_rajt();
-   virtual in_gr_ch& kiir_cel();
-   virtual ~in_gr_ch()
-    { cout << "\nFelszabadítottam az unoka-objektum saját adatának a memóriacímét: "; }
+class in_gr_ch : public in_ch  		/* származtatott unoka osztály deklarációja */
+{
+	private:
+		char nem;
+	public:
+		in_gr_ch() : in_ch(), nem('N') {  }
+		in_gr_ch(string lc, int sw, int cyc, int rn, int dp, int t, int kt, char nm) : \
+				in_ch(lc, sw, cyc, rn, dp, t, kt), nem(nm) { 
+			cout << "\nA paraméteres unoka-objektum létrejött: "; 
+		}
+		virtual in_gr_ch& kiir_rajt();
+		virtual in_gr_ch& kiir_cel();
+		virtual ~in_gr_ch()
+			{ cout << "\nFelszabadítottam az unoka-objektum saját adatának a memóriacímét: "; }
  };
 
 void mind1(tri_race* x_tri);
