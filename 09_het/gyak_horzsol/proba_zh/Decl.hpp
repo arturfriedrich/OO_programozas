4cefc80e7502d547ef9048be7b85ff203f56e26c
Friedrich Artúr <artur.friedrich.harka@gmail.com>
2022-11-03 12:52:45 +0100

    proba_zh második lépcső added

--- 09_het/gyak_horzsol/proba_zh/Decl.hpp
+++ 09_het/gyak_horzsol/proba_zh/Decl.hpp
@@ -3,13 +3,18 @@
 using namespace std;
 
 class Sports {
+    double distance, time, average;
     protected:
-        const int sZ, eZ, oRA, pRC;
-
+        const int sZ, eZ, oRA, pRC;             /* sZ = 100, eZ = 1000, oRA = 3600, pRC = 60 */
+    public:
+        Sports(double dt, double tm) : sZ(100), eZ(1000), oRA(3600), pRC(60) {        /* paraméteres szülő objektum */
+            distance = dt; time = tm; average = 0.0;
+        }
+    
 };
 
 class Swimming : public Sports {
-
+        
 };
 
 class Cycling : public Sports {
