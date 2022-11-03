f306bcce115b3092e7e0a9c17a8b612df608098d
Friedrich Artúr <artur.friedrich.harka@gmail.com>
2022-11-03 13:03:53 +0100

    proba_zh harmadik lépcső added

--- 09_het/gyak_horzsol/proba_zh/Decl.hpp
+++ 09_het/gyak_horzsol/proba_zh/Decl.hpp
@@ -9,12 +9,19 @@ class Sports {
     public:
         Sports(double dt, double tm) : sZ(100), eZ(1000), oRA(3600), pRC(60) {        /* paraméteres szülő objektum */
             distance = dt; time = tm; average = 0.0;
+            cout << "\nAz alapértelmezett szülő-objektum létrejött: ";
         }
-    
+        /* harmadik lépcső */
+        double getDistance() const { return distance; }
+        double getTime() const { return time; }
+        double getAverage() const { return average; }
+        void setAverage(double avg) { average = avg; }
+        virtual void computeSaveAndPrintAverage() {}
+        virtual ~Sports() {}
 };
 
 class Swimming : public Sports {
-        
+
 };
 
 class Cycling : public Sports {
