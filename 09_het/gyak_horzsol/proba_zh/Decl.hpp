60c36fa46bb25309f6feae9a3d682e18033302a3
Friedrich Artúr <artur.friedrich.harka@gmail.com>
2022-11-03 12:42:01 +0100

    proba_zh első lépcső added

--- 09_het/gyak_horzsol/proba_zh/Decl.hpp
+++ 09_het/gyak_horzsol/proba_zh/Decl.hpp
@@ -1,3 +1,24 @@
 #include <iostream>
 
 using namespace std;
+
+class Sports {
+    protected:
+        const int sZ, eZ, oRA, pRC;
+    public:
+        int tavolsag, ido;
+
+
+};
+
+class Swimming : public Sports {
+
+};
+
+class Cycling : public Sports {
+
+};
+
+class Running : public Sports {
+
+};
