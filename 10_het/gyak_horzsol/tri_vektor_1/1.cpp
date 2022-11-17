70572668e966f936f44ec7f1515c4ab1bb263597
Friedrich Artúr <artur.friedrich.harka@gmail.com>
2022-11-17 12:49:54 +0100

    tri_vektor_1 1.cpp kommentelve

--- 10_het/gyak_horzsol/tri_vektor_1/1.cpp
+++ 10_het/gyak_horzsol/tri_vektor_1/1.cpp
@@ -4,22 +4,32 @@ void h_m_s::kiir_i1() const
  { int f=0;
    for(auto itb : hpmp)
       {
+        /* beállítom a mezőszélességet, hogy a helyiértékek jó helyen legyenek */
        cout << setw(2) << itb;
+       /* az idők között : van, azonban a sor végén mást kell kiírni */
        if(f!=2) { cout << ":"; f++; }
        else cout << " [h:m:s]\n";
       }
  }
 
 void in_ch::kiir_i4()
+/* kiolvasgatjuk a tagváltozókat és feltöltjük ezt a vektort */
  { sps = {tagok.getSwim(),tagok.getCycle(),tagok.getDepo(),tagok.getSwim()+tagok.getCycle()+tagok.getDepo(),\
              tagok.getRun(),tagok.getIj(),getSum()};  
    cout << "Licence: " << tagok.getLic() << endl;
+   /* az sps vektort járja be ez a for ciklus, mindkét vektor ugyanolyan hosszú,
+      ezért mindegy melyiket vizsgáljuk, HA két olyan vektor van aminek a hossza,
+      nem egyezik meg, akkor a rövidebb végénél kell megállítani, különben memória
+      szeméttel kezdené el feltölteni */
    for(it=sps.begin(), j_it=jelek.begin(); it!=sps.end(); it++, j_it++)
     {
+        /* itt megkapja az első értéket */
      hpmp.clear(); s_hms(*it);
+     /* kiírja az éppen aktuális jelet */
      cout << *j_it << " ";
      kiir_i1();
     }
+    /* ha végigért a sportágakon, akkor egy getteren keresztül kiírja a kategóriákat is */
    cout << "Kategória: " << getKat() << endl;
  }
  
@@ -27,6 +37,7 @@ in_ch& in_ch::kiir_cel()
  { kiir_i4(); return *this; }
 
 in_gr_ch& in_gr_ch::kiir_cel()
+/* itt  is hasonló ami történek, csak itt a kiir_i4-et hívja meg, ami getteli a nemet */
  { kiir_i4();
    cout << "Nem: " << getNem() << endl;
    return *this; }
@@ -35,6 +46,7 @@ tri_race& tri_race::setSum()
  { sum=tagok.getSwim()+tagok.getCycle()+tagok.getRun()+tagok.getDepo()-tagok.getIj();
    return *this; }
 
+/* nem pointert kap hanem vektort */
 void mind1(vector<in_ch*> x_tri)
  { cout << endl;
    for(auto it : x_tri) {
@@ -55,8 +67,10 @@ void mind1(vector<in_ch*> x_tri)
    it->setSum().kiir_cel(); }
  }
    
+   /* ez nem egy tagfüggvény */
 void felsz(vector<in_ch*> x_tri)
  { for(vector<in_ch*>::reverse_iterator it=x_tri.rbegin();
    it != x_tri.rend(); it++)
+   /* a delete utasítás hívogatja az osztályok destruktorát */
     { delete *it; cout << *it << endl; *it=0; }
  }
