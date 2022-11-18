5728aa91c7b4cb97669233f5267be794d2ff1905
Artúr Friedrich <arturfriedrich@Arturs-MacBook-Air.local>
2022-11-18 16:59:32 +0100

    7. heti csapoadam gyakorlat added

---
+++ 07_het/gyak_csapoadam/ora-007-dinamikuspolimorfizmusShapes/shapes.h
@@ -0,0 +1,55 @@
+#pragma once
+#include <math.h>
+#include <string>
+
+class Shape {
+	std::string type;
+public:
+	Shape (const std::string& type) : type(type) {}
+	std::string printType() { return type; }
+
+	virtual double area() = 0;
+	virtual int getSarkokSzama() = 0;
+	
+	bool isLargerThan(Shape* other) {
+		return area() > other->area();
+	}
+};
+
+class Point2D {
+	int x, y;
+public:
+	Point2D(int xc, int yc) : x(xc), y(yc) {}
+	double distance(const Point2D& other) {
+		return sqrt(pow(x - other.x, 2) + pow(y - other.y, 2));
+	}
+};
+
+class Rectangle : public Shape {
+	Point2D balf, bala, jobbf, jobba;
+public:
+	Rectangle(Point2D bf, Point2D ba, Point2D ja, Point2D jf) :
+		Shape("Teglalap"), balf(bf), bala(ba), jobbf(jf), jobba(ja)
+	{}
+	Rectangle(Rectangle& other) = delete;
+	Rectangle& operator=(Rectangle& other) = delete;
+	int getSarkokSzama() { return 4; }
+	double area() override {
+		return balf.distance(bala) * bala.distance(jobba);
+	}
+};
+
+class Circle : public Shape {
+	Point2D kozepe;
+	int sugar;
+public:
+	Circle(Point2D center, int rad) :
+		Shape("Kor"), kozepe(center), sugar(rad)
+	{}
+	Circle(Circle& other) = delete;
+	Circle& operator=(Circle& other) = delete;
+	int getSarkokSzama() { return 0; }
+	double area() override {
+		return sugar * sugar * 3.14;
+	}
+};
