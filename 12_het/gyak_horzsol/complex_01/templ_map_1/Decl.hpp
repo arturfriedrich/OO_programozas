be73f7c85e43a91ca59ef68b80575bbf96ed120a
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-11-24 14:25:50 +0100

    kommented

--- 12_het/gyak_horzsol/complex_01/templ_map_1/Decl.hpp
+++ 12_het/gyak_horzsol/complex_01/templ_map_1/Decl.hpp
@@ -4,6 +4,6 @@
 
 using namespace std;
 
-typedef pair<string, int> sps;
-map<string, int> hpmp;
-map<string, int>::iterator ke;
+typedef pair<string, int> sps;      // ez egy osztály, objektum, nem konténer, struktúra sablon, amit be tudok építeni egy konténerbe vagy map-be
+map<string, int> hpmp;              // asszociatív tömb létrehozása, kulcsokon keresztül lehet elérni az adatokat
+map<string, int>::iterator ke;      // a string a kulcs, az int az adat
--- 12_het/gyak_horzsol/complex_01/templ_map_1/main.cpp
+++ 12_het/gyak_horzsol/complex_01/templ_map_1/main.cpp
@@ -2,39 +2,39 @@
  
 int main(void)
 {
-	hpmp["F: "] = 2300;
-	for (auto it : hpmp)
+	hpmp["F: "] = 2300;         // létrehozás, meg kell adni a kulcsot, majd az értéket
+	for (auto it : hpmp)        // egy iterátorral végigmegyünk a hpmp-n, a first a kulcs a second az adat
 	{ cout << it.first << it.second << "\n\n"; }
-	hpmp = { {"Ú: ",1200}, {"B: ",4200} }; // Felüldefiniálás!
+	hpmp = { {"Ú: ",1200}, {"B: ",4200} }; // Felüldefiniálás (megsemmisítés)
 	for (auto it : hpmp)
 	 {	cout << it.first << it.second << "\n"; }
 	cout << "\n" << hpmp.size();
-	hpmp.clear();
+	hpmp.clear();       // tudjuk törölni
 	cout << "\n" << hpmp.size() << "\n\n";
 	hpmp = { {"F: ",0}, {"Ú: ",1200}, {"B: ",4200} };
 	for (auto it : hpmp)
 	 {	cout << it.first << it.second << "\n"; }
-	cout << "\n" << hpmp.size() << "\n\n";
+	cout << "\n" << hpmp.size() << "\n\n";      // .size -- méret lekérdezése
 	hpmp["D: "] = 123; // Beszúrás!
 	hpmp["D: "] = 123; // Nem lehet ismétlődés! => Ez egy felülírás!
 	hpmp["F: "] = 2300;   // Felülírás!
 	auto it = hpmp.begin();
-	//hpmp.insert(it,pair<string, int>("Ú+B+D: ", 1200 + 4200 + 123));
-	//hpmp.insert(make_pair("Ú+B+D: ",1200+4200+123));
-	hpmp.insert(sps("Ú+B+D: ",1200+4200+123));
+	hpmp.insert(it,pair<string, int>("Ú+B+D: ", 1200 + 4200 + 123));        // itt definiálni kell, hogy string vagy int
+	hpmp.insert(make_pair("Ú+B+D: ",1200+4200+123));            // nem kell definiálni, hogy string vagy int, auto-val működik
+	hpmp.insert(sps("Ú+B+D: ",1200+4200+123));      // az insert paranccsal is be lehet szúrni, megadjuk azt, hogy hova szúrjuk be, aztán azt, hogy mikor
 	for (auto it : hpmp)
 	 {	cout << it.first << it.second << "\n"; }
 	cout << "\n" << hpmp.size() << "\n\n";
-	for (auto r_it = hpmp.rbegin(); r_it != hpmp.rend(); r_it++)
+	for (auto r_it = hpmp.rbegin(); r_it != hpmp.rend(); r_it++)        // fordított kiírás
 	 { cout << r_it->first << r_it->second << "\n";	} cout << "\n";
-	hpmp.erase(hpmp.find("D: "));
+	hpmp.erase(hpmp.find("D: "));                   // lehet törölni, a find-al megkeressük a kulcs alapján az elemet, a find visszaadja a címét, majd az erase törli
 	for (auto it : hpmp)
 	 {	cout << it.first << it.second << "\n"; }
 	if (hpmp.count("Ú: ")) {
       cout << "\nLétezik az elem!\n";
-      ke=hpmp.find("Ú: ");
-      cout << ke->first << ke->second << "\n";
-      ke->second = 1100; // Nem kell felüldefiniálni a teljes pair-t!
+      ke=hpmp.find("Ú: ");      // az Ú címe
+      cout << ke->first << ke->second << "\n";      // kiírjuk a kulcsot, majd az adatot
+      ke->second = 1100; // Nem kell felüldefiniálni a teljes pair-t!       --- felülírjuk az adatot
       cout << ke->first << ke->second << "\n";}
 	else cout << "\nNem létezik az elem!";
   ke=hpmp.find("B: ");
