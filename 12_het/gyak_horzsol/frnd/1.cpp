7100ecd6d419d012c1030a3201c99d0113005038
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-11-30 16:58:25 +0100

    frnd refactored

--- 12_het/gyak_horzsol/frnd/1.cpp
+++ 12_het/gyak_horzsol/frnd/1.cpp
@@ -1,71 +1,67 @@
 #include "Decl.hpp"
 
-void h_m_s::kiir_i1() const
- { int f=0;
-   for(auto itb : hpmp)
-      {
+void h_m_s::kiir_i1() const { 
+	int f=0;
+   	for(auto itb : hpmp) {
        cout << setw(2) << itb;
        if(f!=2) { cout << ":"; f++; }
        else cout << " [h:m:s]\n";
-      }
- }
+	}
+}
 
-void in_ch::kiir_i4()
- { sps = {tagok.getSwim(),tagok.getCycle(),tagok.getDepo(),tagok.getSwim()+tagok.getCycle()+tagok.getDepo(),\
-             tagok.getRun(),tagok.getIj(),getSum()};  
-   cout << "Licence: " << tagok.getLic() << endl;
-   for(it=sps.begin(), j_it=jelek.begin(); it!=sps.end(); it++, j_it++)
-    {
-     hpmp.clear(); s_hms(*it);
-     cout << *j_it << " "; 
-     kiir_i1();
-    }
-   cout << "Kategória: " << getKat() << endl;
- }
+void in_ch::kiir_i4() { 
+	sps = { tagok.getSwim(), tagok.getCycle(), tagok.getDepo(), tagok.getSwim()+tagok.getCycle()+tagok.getDepo(), tagok.getRun(), tagok.getIj(), getSum()};  
+	cout << "Licence: " << tagok.getLic() << endl;
+	for(it=sps.begin(), j_it=jelek.begin(); it!=sps.end(); it++, j_it++) {
+		hpmp.clear(); s_hms(*it);
+		cout << *j_it << " "; 
+		kiir_i1();
+	}
+	cout << "Kategória: " << getKat() << endl;
+}
  
 in_ch& in_ch::kiir_cel()
- { kiir_i4(); return *this; }
+ 	{ kiir_i4(); return *this; }
 
-in_gr_ch& in_gr_ch::kiir_cel()
- { kiir_i4();
-   cout << "Nem: " << getNem() << endl;
-   return *this; }
+in_gr_ch& in_gr_ch::kiir_cel() { 
+	kiir_i4();
+	cout << "Nem: " << getNem() << endl;
+	return *this; 
+}
   
-tri_race& tri_race::setSum()
- { sum=tagok.getSwim()+tagok.getCycle()+tagok.getRun()+tagok.getDepo()-tagok.getIj();
-   return *this; }
+tri_race& tri_race::setSum() { 
+	sum=tagok.getSwim()+tagok.getCycle()+tagok.getRun()+tagok.getDepo()-tagok.getIj();
+   	return *this; 
+}
 
-void mind1(vector<in_ch*> x_tri)
- { cout << endl;
-   for(auto it : x_tri) {
-   in_gr_ch* rp=dynamic_cast<in_gr_ch*>(it);
-   if(rp!=nullptr) { cout << "\nUnoka ";  }
-   else
-     {
-      in_ch* rp=dynamic_cast<in_ch*>(it); 
-      if(rp!=nullptr) { cout << "\nGyerek "; }
-      else
-       {
-        tri_race* rp=dynamic_cast<tri_race*>(it);
-        cout << "\nSzülő ";
-       }
-     }
-   cout << "rajt idő: ";
-   it->kiir_i1();
-   it->setSum().kiir_cel(); } 
- }
+void mind1(vector<in_ch*> x_tri) { 
+	cout << endl;
+	for(auto it : x_tri) {
+		in_gr_ch* rp=dynamic_cast<in_gr_ch*>(it);
+		if(rp!=nullptr) { cout << "\nUnoka ";  }
+		else {
+			in_ch* rp=dynamic_cast<in_ch*>(it); 
+			if(rp!=nullptr) { cout << "\nGyerek "; }
+			else {
+				tri_race* rp=dynamic_cast<tri_race*>(it);
+				cout << "\nSzülő ";
+			}
+		}
+		cout << "rajt idő: ";
+		it->kiir_i1();
+		it->setSum().kiir_cel(); 
+		} 
+}
    
-void felsz(vector<in_ch*> x_tri)
- { for(vector<in_ch*>::reverse_iterator it=x_tri.rbegin();
-   it != x_tri.rend(); it++)
-    { delete *it; cout << *it << endl; *it=0; } 
+void felsz(vector<in_ch*> x_tri) { 
+	for(vector<in_ch*>::reverse_iterator it=x_tri.rbegin(); it != x_tri.rend(); it++)
+		{ delete *it; cout << *it << endl; *it=0; } 
  }
 
-void frnd::olvas(in_ch* x_tri)
- { 
+void frnd::olvas(in_ch* x_tri) { 
     cout << "\nLicence: " << x_tri->tagok.lic << "\nKategória: " <<  x_tri->getKat() << "\n";
     cout << "Cél idő: " << x_tri->sum << "\n";
     x_tri->tagok.lic="X_Y_0000"; x_tri->setKat(3); x_tri->sum=9000;
     cout << "\nLicence: " << x_tri->tagok.lic << "\nKategória: " << x_tri->getKat() << "\n";
     cout << "Cél idő: " << x_tri->sum << "\n";
- }
+}
--- 12_het/gyak_horzsol/frnd/main.cpp
+++ 12_het/gyak_horzsol/frnd/main.cpp
@@ -1,18 +1,18 @@
-#include "Decl.hpp"
+#include "1.cpp"
 
 int main(void)
- {
-  vector<in_ch*> objs; 
-  in_ch* alap=new in_ch; objs.push_back(alap); cout << "alap = " << alap << endl;
-  in_ch* A_tri=new in_ch("M_V_1357",1200,4200,2300,123,60,6); objs.push_back(A_tri); cout << "A_tri = " << A_tri << endl;
-  in_ch* B_tri=new in_ch("V_G_2468",1300,4300,2400,125,0,6); objs.push_back(B_tri); cout << "B_tri = " << B_tri << endl;
-  in_ch* C_tri=new in_ch("B_G_9048",1400,4400,2400,130,90,6); objs.push_back(C_tri); cout << "C_tri = " << C_tri << endl;
-  in_ch* D_tri=new in_ch("C_H_1234",1450,4400,2400,130,30,5); objs.push_back(D_tri); cout << "D_tri = " << D_tri << endl;
-  in_ch* E_tri=new in_gr_ch("G_R_4321",1500,4450,2350,120,0,4,'F'); objs.push_back(E_tri); cout << "E_tri = " << E_tri << endl;
-  mind1(objs);
-  /* az olvas metódust csak objektum hívással tudjuk futtatni */
-  frnd f;
-  f.olvas(A_tri);
-  felsz(objs);
-  return 0;
+{
+	vector<in_ch*> objs; 
+	in_ch* alap=new in_ch; objs.push_back(alap); cout << "alap = " << alap << endl;
+	in_ch* A_tri=new in_ch("M_V_1357",1200,4200,2300,123,60,6); objs.push_back(A_tri); cout << "A_tri = " << A_tri << endl;
+	in_ch* B_tri=new in_ch("V_G_2468",1300,4300,2400,125,0,6); objs.push_back(B_tri); cout << "B_tri = " << B_tri << endl;
+	in_ch* C_tri=new in_ch("B_G_9048",1400,4400,2400,130,90,6); objs.push_back(C_tri); cout << "C_tri = " << C_tri << endl;
+	in_ch* D_tri=new in_ch("C_H_1234",1450,4400,2400,130,30,5); objs.push_back(D_tri); cout << "D_tri = " << D_tri << endl;
+	in_ch* E_tri=new in_gr_ch("G_R_4321",1500,4450,2350,120,0,4,'F'); objs.push_back(E_tri); cout << "E_tri = " << E_tri << endl;
+	mind1(objs);
+	/* az olvas metódust csak objektum hívással tudjuk futtatni */
+	frnd f;
+	f.olvas(A_tri);
+	felsz(objs);
+	return 0;
 }
