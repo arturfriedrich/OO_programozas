9980735464cdc28f0366e0db1e28871acb246441
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2022-12-08 16:55:48 +0100

    refactored

--- 13_het/gyak_horzsol/complex_02/main.cpp
+++ 13_het/gyak_horzsol/complex_02/main.cpp
@@ -1,15 +1,15 @@
 #include "1.cpp"
 
 int main(void)
- {
-  in_ch* base=new in_ch(); base->b_mem(base,0);
-  base->ch_objs();
-  base->ch_objs("M_V_1357",1200,4200,2300,123,60,6);
-  base->ch_objs("V_G_2468",1300,4300,2400,125,0,6);
-  base->ch_objs("B_G_9048",1400,4400,2400,130,90,6);
-  base->ch_objs("C_H_1234",1450,4400,2400,130,30,5);
-  base->ch_objs("G_R_4321",1500,4450,2350,120,0,4,'F');
-  base->mind1();
-  delete base; base->b_mem(base,1);
-  return 0;
+{
+	in_ch* base=new in_ch(); base->b_mem(base,0);
+	base->ch_objs();
+	base->ch_objs("M_V_1357",1200,4200,2300,123,60,6);
+	base->ch_objs("V_G_2468",1300,4300,2400,125,0,6);
+	base->ch_objs("B_G_9048",1400,4400,2400,130,90,6);
+	base->ch_objs("C_H_1234",1450,4400,2400,130,30,5);
+	base->ch_objs("G_R_4321",1500,4450,2350,120,0,4,'F');
+	base->mind1();
+	delete base; base->b_mem(base,1);
+	return 0;
 }
