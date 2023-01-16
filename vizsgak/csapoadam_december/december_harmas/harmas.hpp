b1e51a1f356a2e985fdf4f076f4b8e4f0ddf0ac6
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-01-16 14:11:38 +0100

    hármas szint added

--- vizsgak/csapoadam_december/december_harmas/harmas.hpp
+++ vizsgak/csapoadam_december/december_harmas/harmas.hpp
@@ -5,55 +5,52 @@
 
 class EtlapElem
 {
-protected:
-    std::string nev;
-    int ar;
-
-public:
-    EtlapElem(std::string nev, int ar) : nev(nev), ar(ar) {}
-    virtual ~EtlapElem() {}
-    virtual void print() = 0;
+    protected:
+        std::string nev;
+        int ar;
+    public:
+        EtlapElem(std::string nev, int ar) : nev(nev), ar(ar) {}
+        virtual ~EtlapElem() {}
+        virtual void print() = 0;
 };
 
 class Leves : public EtlapElem
 {
-public:
-    Leves(std::string nev, int ar) : EtlapElem(nev, ar) {}
-    void print()
-    {
-        std::cout << nev << " - " << ar << " Ft" << std::endl;
-    }
+    public:
+        Leves(std::string nev, int ar) : EtlapElem(nev, ar) {}
+        void print()
+        {
+            std::cout << nev << " - " << ar << " Ft" << std::endl;
+        }
 };
 
 class Foetel : public EtlapElem
 {
     std::string koret;
-
-public:
-    Foetel(std::string nev, std::string koret, int ar) : EtlapElem(nev, ar), koret(koret) {}
-    void print()
-    {
-        std::cout << nev;
-        std::cout << " " << koret << " korettel - ";
-        std::cout << ar << " Ft" << std::endl;
-    }
+    public:
+        Foetel(std::string nev, std::string koret, int ar) : EtlapElem(nev, ar), koret(koret) {}
+        void print()
+        {
+            std::cout << nev;
+            std::cout << " " << koret << " korettel - ";
+            std::cout << ar << " Ft" << std::endl;
+        }
 };
+
+/* 3-as szint */
 
 class Etlap
 {
-    std::vector<EtlapElem *> elemek;
+    std::vector<EtlapElem *> elemek;        // vektor létrehozása az étlapon szereplő elemeknek
 
-public:
-    void hozzaad(EtlapElem *ee)
-    {
-        elemek.push_back(ee);
-    }
-    void print()
-    {
-        std::cout << "Etlap:" << std::endl;
-        for (EtlapElem *ee : elemek)
-        {
-            ee->print();
+    public:
+        void hozzaad(EtlapElem *ee) {       // metódus, ami hozzáadja az étlephoz az adott elemet
+            elemek.push_back(ee);           // a vektorba pusholja
         }
-    }
+        void print() {
+            std::cout << "Etlap:" << std::endl;
+            for (EtlapElem *ee : elemek) {  // végig iterál az étlap vektoron és kiírja az elemeit a print() metúdussal
+                ee->print();
+            }
+        }
 };
--- vizsgak/csapoadam_december/december_harmas/main.cpp
+++ vizsgak/csapoadam_december/december_harmas/main.cpp
@@ -7,19 +7,17 @@
 
 int main()
 {
-    static_assert(std::is_abstract<EtlapElem>(), "Hiba! EtlapElem osztaly nem absztrakt!");     // ellenőrzi, hogy a megadott osztály absztrakt-e
-    EtlapElem *ee1 = new Leves("Magocskas leves", 450);                         // példányosítás
-    EtlapElem *ee2 = new Leves("Frankfurti leves", 800);
-    EtlapElem *ee3 = new Foetel("Paprikas csirke", "rizs", 1200);
-    EtlapElem *ee4 = new Foetel("Paprikas csirke", "hasabburgonya", 1500);
-    ee1->print();           // a példányosított osztáylokat kiírja
-    ee2->print();
-    ee3->print();
-    ee4->print();
+    static_assert(std::is_abstract<EtlapElem>(), "Hiba! EtlapElem osztaly nem absztrakt!");
 
-    delete ee1;             // felszabadítja a memóriát
-    delete ee2;
-    delete ee3;
-    delete ee4;
+    Etlap e1;       // létrehozza az Etlap menű vektort
+    e1.hozzaad(new Leves("Magocskas leves", 450));              // hozzáadja az elemeket  hozzaad() metódussal
+    e1.hozzaad(new Leves("Frankfurti leves", 800));
+    e1.hozzaad(new Foetel("Paprikas csirke", "rizs", 1200));
+
+    Etlap e2(e1);   // a második étlapot létrehozza, és azonnal meg is adja neki az elsőt
+    e2.hozzaad(new Foetel("Paprikas csirke", "hasabburgonya", 1500));
+
+    e2.print();     // kiírja a második étlapot
+
     return 0;
 }
