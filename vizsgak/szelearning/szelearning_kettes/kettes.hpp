024f69183a82f82801e1cf85f9d9303eb559e2e0
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-01-17 11:18:26 +0100

    szelearning_kettes initial setup

---
+++ vizsgak/szelearning/szelearning_kettes/kettes.hpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <typeinfo>
+
+class Oktato ...
+
+class Professzor ...
+
+class Tanarseged ...
---
+++ vizsgak/szelearning/szelearning_kettes/main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <typeinfo>
+
+#include "kettes.hpp"
+
+int main()
+{
+    std::vector<Oktato*> oktatok;
+
+    static_assert(std::is_abstract<Oktato>(), "Hiba! Oktato osztaly nem absztrakt!");
+
+    Oktato* tanar1 = new Professzor("Nagy", "Agnes");
+    Oktato* tanar2 = new Tanarseged("Kis", "Bela");
+    
+    oktatok.push_back(tanar1);
+    oktatok.push_back(tanar2);
+
+    for (Oktato* op : oktatok) {
+        op->print();
+    }
+
+    delete tanar1;
+    delete tanar2;
+
+    return 0;
+}
