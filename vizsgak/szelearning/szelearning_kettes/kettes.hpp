58f22f05cb0a833aec8b65949f7dcd5eec904a34
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-01-17 11:32:11 +0100

    kettes szint added

--- vizsgak/szelearning/szelearning_kettes/kettes.hpp
+++ vizsgak/szelearning/szelearning_kettes/kettes.hpp
@@ -3,8 +3,30 @@
 #include <vector>
 #include <typeinfo>
 
-class Oktato ...
+class Oktato
+{
+    protected:
+        std::string lastName, firstName;
+    public:
+        Oktato(std::string lastName, std::string firstName) : lastName(lastName), firstName(firstName) { }
+        virtual ~Oktato() { }
+        virtual void print() = 0;
+};
 
-class Professzor ...
+class Professzor : public Oktato
+{
+    public:
+        Professzor(std::string lastName, std::string firstName) : Oktato(lastName, firstName) { }
+        void print() {
+            std::cout << "Professzor: " << lastName << ", " << firstName << std::endl;
+        }
+};
 
-class Tanarseged ...
+class Tanarseged : public Oktato
+{
+    public:
+        Tanarseged(std::string lastName, std::string firstName) : Oktato(lastName, firstName) { }
+        void print() {
+            std::cout << "Tanarseged: " << lastName << ", " << firstName << std::endl;
+        }
+};
