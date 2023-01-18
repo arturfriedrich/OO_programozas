d5ec2c04871fd445e6d141d49e258af98ef8b430
Artúr Friedrich <artur.friedrich.harka@gmail.com>
2023-01-18 11:43:55 +0100

    harmas_3 addded

--- vizsgak/szelearning/szelearning_harmadik_3/harmadik_3.hpp
+++ vizsgak/szelearning/szelearning_harmadik_3/harmadik_3.hpp
@@ -11,10 +11,38 @@ class Termek
         std::string mertekegyseg;
     public:
         Termek(float mennyiseg, int ar, std::string mertekegyseg) : mennyiseg(mennyiseg), ar(ar), mertekegyseg(mertekegyseg) { }
-        virtual ~Termek() { }                       // ez egy üres, virtuális destruktor
+        virtual ~Termek() { }
         virtual double getTotal() {                 // visszaadja, a termék árát
             return mennyiseg * ar;
         }
+        void setAr(int newAr) {                     // nullázni lehet vele a termék árát, mintha kivettük volna a kosárból
+            ar = newAr;
+        }
+};
+
+class Barack : public Termek
+{
+    public:
+        Barack(double mennyiseg, int ar, std::string mertekegyseg) : Termek(mennyiseg, ar, mertekegyseg) { }
+};
+
+class Tej : public Termek
+{
+    double zsirtartalom;            // azért kell külön zsirtaltarlam létrehozni, mivel a Termek csak 3 paramétert fogad, itt viszont 4 van
+
+    public:
+        Tej(double m, double zs, int ar, std::string megy) : Termek(m, ar, megy), zsirtartalom(zs) { }
+};
+
+class TrappistaSajt : public Termek
+{
+    double kedvezmeny;              // azért kell külön kedvezmeny létrehozni, mivel a Termek csak 3 paramétert fogad, itt viszont 4 van
+
+public:
+    TrappistaSajt(double m, int ar, std::string megy, double kedv) : Termek(m, ar, megy), kedvezmeny(kedv) {}
+    double getTotal() {
+        return mennyiseg * ar * (1.0 - kedvezmeny);
+    }
 };
 
 class Kosar
@@ -45,29 +73,12 @@ class Kosar
             }
             std::cout << " = " << teljes_ertek << std::endl;
         }
-};
-
-class Barack : public Termek
-{
-    public:
-        Barack(double mennyiseg, int ar, std::string mertekegyseg) : Termek(mennyiseg, ar, mertekegyseg) { }
-};
-
-class Tej : public Termek
-{
-    double zsirtartalom;            // azért kell külön zsirtaltarlam létrehozni, mivel a Termek csak 3 paramétert fogad, itt viszont 4 van
-
-    public:
-        Tej(double m, double zs, int ar, std::string megy) : Termek(m, ar, megy), zsirtartalom(zs) { }
-};
-
-class TrappistaSajt : public Termek
-{
-    double kedvezmeny;              // azért kell külön kedvezmeny létrehozni, mivel a Termek csak 3 paramétert fogad, itt viszont 4 van
-
-public:
-    TrappistaSajt(double m, int ar, std::string megy, double kedv) : Termek(m, ar, megy), kedvezmeny(kedv) {}
-    double getTotal() {
-        return mennyiseg * ar * (1.0 - kedvezmeny);
-    }
+        void remove(std::string mitToruljunk) {         // ki lehet vele törölni egy elemet
+            for ( Termek *tp : tartalom ){
+                TrappistaSajt *tsp = dynamic_cast<TrappistaSajt *>(tp);     // megadjuk, hogy melyiket szeretnénk törölni
+                if ( tsp ) {
+                    tp->setAr(0);                       // a setAr(0)-val nullázzuk az árát az adott terméknek
+                }
+            } 
+        } 
 };
